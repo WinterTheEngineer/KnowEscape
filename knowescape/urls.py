ed86df28b50ec3342dbe675232dcab23930684a9
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-12-11 05:14:42 +0200

    SEO modifications to templates

--- knowescape/urls.py
+++ knowescape/urls.py
@@ -4,13 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('start-up', views.start_up, name="start-up"),
-    path('contact', views.contact, name="contact"),
-    path('signup/learner', views.learner_signup, name="learner-signup"),
-    path('signup/company', views.company_signup, name="company-signup"),
-    path('enterprise-supplier-dev', views.es_dev, name="es-dev"),
-    path('skills-dev', views.skills_dev, name="skills-dev"),
+    path("", views.index, name="home"),
+
+    # Services / key pages
+    path("start-up-support/", views.start_up, name="startup_support"),
+    path("enterprise-supplier-development/", views.es_dev, name="esd"),
+    path("skills-development/", views.skills_dev, name="skills_development"),
+
+    # Other important actions/pages
+    path("contact/", views.contact, name="contact"),
+    path("signup/learner/", views.learner_signup, name="learner_signup"),
+    path("signup/company/", views.company_signup, name="company_signup"),
 ]
 
 if settings.DEBUG:
